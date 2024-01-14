494e7f9ac7fb226c7f067111cb44c79397b2a4ee
Craftor <craftor@126.com>
2024-01-14 15:53:50 +0800

    add docker build in workspace

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+docker login
+
+# 遍历工作区中的每个应用
+for dir in $(find . -maxdepth 1 -type d ! -name ".*" ! -name "target" -print); do
+    # 检查目录下是否存在 Cargo.toml 文件
+    if [ -f "$dir/Cargo.toml" ]; then
+        # 进入应用的目录
+        cd $dir
+
+        # 使用 cargo 命令和 jq 工具获取应用的名称
+        app_name=$(cargo read-manifest | jq -r .name)
+        # 使用 cargo 命令和 jq 工具获取应用的版本号
+        app_version=$(cargo read-manifest | jq -r .version)
+        
+        # 返回上一层目录
+        cd ..
+
+        # 打包并上传
+        docker buildx build \
+                --tag logicpi/$app_name:$app_version .\
+                --platform linux/amd64 \
+                --push
+        
+    fi
+done
