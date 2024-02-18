9a956268d119ac13d0db8eb98f0aa2573337698e
Craftor <craftor@126.com>
2024-02-18 13:59:46 +0800

    add baiduyun api init

---
+++ baiduyun-api/src/lib.rs
@@ -0,0 +1,4 @@
+pub const APP_ID: &str = "51941956";
+pub const APP_KEY: &str = "EkMjcdUOn0CCsrmraBHRvvGGP62UpgYE";
+pub const SECRET_KEY: &str = "7q4GJsIIcFdVX04NipLJdOxjXph6zqXH";
+pub const SIGN_KEY: &str = "UYq4-phS@YtOlLm^h@WYBv+P@dtfFKP7";
