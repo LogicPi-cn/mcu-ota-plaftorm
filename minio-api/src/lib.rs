648944a8d15303c3483495fd9bb664c43e718bc5
Bryan Xiang <xiangshang@logicpi.cn>
2024-07-28 22:08:50 +0800

    update

--- minio-api/src/lib.rs
+++ minio-api/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod apis;
 
-pub const MINIO_SERVER: &str = "http://db.21up.cn:9000";
-pub const ACCESS_KEY: &str = "KLDzQUrDY7wX4TCd7iiu";
-pub const SECRET_KEY: &str = "ispyS0RdIWoCBCpXXQkBnKIF2oURHJwRgfEcwee6";
-pub const BUCKET_NAME: &str = "ota-files";
+pub const MINIO_SERVER: &str = "http://ota.logicpi.cn:9000";
+pub const ACCESS_KEY: &str = "debug";
+pub const SECRET_KEY: &str = "3.1415926";
+pub const BUCKET_NAME: &str = "ota-debug";
--- minio-api/src/main.rs
+++ minio-api/src/main.rs
@@ -1,51 +1,85 @@
-use minio::s3::args::{BucketExistsArgs, MakeBucketArgs, UploadObjectArgs};
-use minio::s3::client::Client;
-use minio::s3::creds::StaticProvider;
-use minio::s3::http::BaseUrl;
-use minio_api::{ACCESS_KEY, BUCKET_NAME, MINIO_SERVER, SECRET_KEY};
+use minio_api::{ACCESS_KEY, SECRET_KEY};
+
+use minio_rsc::client::{BucketArgs, KeyArgs};
+use minio_rsc::error::Result;
+use minio_rsc::provider::StaticProvider;
+use minio_rsc::Minio;
+
+use std::fs::File;
+use std::io::Read;
+
+async fn example() -> Result<()> {
+    let provider = StaticProvider::new(ACCESS_KEY, SECRET_KEY, None);
+    let minio = Minio::builder()
+        .endpoint("ota.logicpi.cn:9000")
+        .provider(provider)
+        .secure(false)
+        .build()
+        .unwrap();
+    let (_buckets, _owner) = minio.list_buckets().await?;
+
+    minio.make_bucket(BucketArgs::new("bucket1"), false).await?;
+    minio.make_bucket("bucket2", true).await?;
+
+    minio
+        .put_object("bucket1", "hello.txt", "hello minio!".into())
+        .await?;
+    minio.stat_object("bucket1", "hello.txt").await?;
+    minio.get_object("bucket1", "hello.txt").await?;
+    let key = KeyArgs::new("hello.txt")
+        .version_id(Some("cdabf31a-9752-4265-b137-6b3961fbaf9b".to_string()));
+    minio.get_object("bucket1", key).await?;
+    minio.remove_object("bucket1", "hello.txt").await?;
+
+    let bucket2 = minio.bucket("bucket2");
+    bucket2
+        .put_object("hello.txt", "hello minio!".into())
+        .await?;
+    bucket2.stat_object("hello.txt").await?;
+    bucket2.get_object("hello.txt").await?;
+    bucket2.remove_object("hello.txt").await?;
+
+    // if fs-tokio feature enabled
+    // download file to local
+    minio
+        .fget_object("bucket1", "hello.txt", "local.txt")
+        .await?;
+    // // upload file to minio
+    minio
+        .fput_object("bucket1", "hello.txt", "local.txt")
+        .await?;
+
+    // minio.remove_bucket("bucket1").await?;
+    // minio.remove_bucket("bucket2").await?;
+
+    Ok(())
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let base_url = MINIO_SERVER.parse::<BaseUrl>()?;
-
-    let static_provider = StaticProvider::new(ACCESS_KEY, SECRET_KEY, None);
-
-    let client = Client::new(
-        base_url.clone(),
-        Some(Box::new(static_provider)),
-        None,
-        None,
-    )
-    .unwrap();
-
-    // Check 'asiatrip' bucket exist or not.
-    let exists = client
-        .bucket_exists(&BucketExistsArgs::new(&BUCKET_NAME).unwrap())
-        .await
+async fn main() -> Result<()> {
+    let provider = StaticProvider::new(ACCESS_KEY, SECRET_KEY, None);
+    let minio = Minio::builder()
+        .endpoint("ota.logicpi.cn:9000")
+        .provider(provider)
+        .secure(false)
+        .build()
         .unwrap();
 
-    // Make 'asiatrip' bucket if not exist.
-    if !exists {
-        client
-            .make_bucket(&MakeBucketArgs::new(&BUCKET_NAME).unwrap())
-            .await
-            .unwrap();
-    }
+    // 打开文件
+    let mut file =
+        File::open("/home/craftor/mcu-ota-platform/file/1987-0.2.0.bin").expect("无法打开文件");
 
-    // Upload '/home/user/Photos/asiaphotos.zip' as object name
-    // 'asiaphotos-2015.zip' to bucket 'asiatrip'.
-    client
-        .upload_object(
-            &mut UploadObjectArgs::new(
-                &BUCKET_NAME,
-                "1987-0.2.0.bin",
-                "/root/proj/mcu-ota-platform/file/1987-0.2.0.bin",
-            )
-            .unwrap(),
-        )
-        .await
-        .unwrap();
+    // 读取文件内容到一个字节数组中
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("无法读取文件");
 
-    println!("successfully uploaded");
+    minio
+        .put_object("ota-debug", "1987-0.2.0.bin", buffer.into())
+        .await?;
+
+    minio
+        .fget_object("ota-debug", "1987-0.2.0.bin", "download.bin")
+        .await?;
+
     Ok(())
 }
