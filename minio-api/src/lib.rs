009d6a9686c942584083cbe49323d60b992064cd
Bryan Xiang <xiangshang@logicpi.cn>
2026-01-11 22:16:50 +0800

    update

--- minio-api/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod apis;
-
-pub const MINIO_SERVER: &str = "http://ota.logicpi.cn:9000";
-pub const ACCESS_KEY: &str = "debug";
-pub const SECRET_KEY: &str = "3.1415926";
-pub const BUCKET_NAME: &str = "ota-debug";
